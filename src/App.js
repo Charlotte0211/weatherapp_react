5ed29cce2928bce93d48e250b281f0d1d5b681ce
Charlotte0211 <69457852+Charlotte0211@users.noreply.github.com>
2024-10-01 14:42:04 +0100

    Added GitHub repository URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1> Weather app</h1>
+      <footer>
+        This project was coded by Charlotte Rivière and is{" "}
+        <a
+          href="https://github.com/Charlotte0211/new-weather_app"
+          target="_blank"
+        >
+          {" "}
+          open-sourced on GitHub.
+        </a>
+      </footer>
+    </div>
+  );
 }
