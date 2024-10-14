d7f830c45fb34452e913426e215b19f23fc7969d
Charlotte0211 <69457852+Charlotte0211@users.noreply.github.com>
2024-10-14 15:35:54 +0100

    Add bootstrap to my react project
    Went to https://create-react-app.dev/docs/adding-bootstrap/

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather app</h1>
-      <footer>
-        This project was coded by Charlotte Rivière and is{" "}
-        <a
-          href="https://github.com/Charlotte0211/new-weather_app"
-          target="_blank"
-        >
-          {" "}
-          open-sourced on GitHub.
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather app</h1>
+        <footer>
+          This project was coded by Charlotte Rivière and is{" "}
+          <a
+            href="https://github.com/Charlotte0211/new-weather_app"
+            target="_blank"
+          >
+            {" "}
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
